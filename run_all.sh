10f52c4a322865fb4f0637cf4136b2a25044cf19
Josh <44043414+jgard1@users.noreply.github.com>
2021-03-02 21:41:54 -0500

    dank

--- run_all.sh
+++ run_all.sh
@@ -4,10 +4,10 @@ CIFAR = "cifar_"
 MNIST = "mnist_"
 
 # experiment 1 
-for PRUNER_NAME in rand snip grasp synflow
+for PRUNER_NAME in "rand" "snip" "grasp" "synflow"
 do
   # python main.py --model-class lottery --model vgg16 --dataset cifar10 --experiment singleshot --pruner synflow --compression 1 --expid $CIFAR$PRUNER_NAME --post-epoch 100
-  NAME = $MNIST + $PRUNER_NAME
+  NAME = "$MNIST$PRUNER_NAME"
   python main.py --model-class default --model fc --dataset mnist --experiment singleshot --pruner synflow --compression 1 --expid $NAME --post-epoch 10
 done
 # have to deal with magnitue pruning seperately because the pre-epochs needs to be set to 200
@@ -16,14 +16,14 @@ python main.py --model-class default --model fc --dataset mnist --experiment mul
 
 # second set of experiments 
 MAG = "mag"
-for COMPRESSION in 0.05 0.1 0.2 0.5 1 2      ### Outer for loop ###
+for COMPRESSION in "0.05" "0.1" "0.2" "0.5" "1" "2"      ### Outer for loop ###
 do
 
-    for PRUNER_NAME in rand snip grasp synflow ### Inner for loop ###
+    for PRUNER_NAME in "rand" "snip" "grasp" "synflow" ### Inner for loop ###
     do
-          NAME = $COMPRESSION + $CIFAR + $PRUNER_NAME
+          NAME = "$COMPRESSION$CIFAR$PRUNER_NAME"
           python main.py --model-class lottery --model vgg16 --dataset cifar10 --experiment singleshot --pruner synflow --compression $COMPRESSION --expid $NAME --post-epoch 100
     done
-    MAG_NAME = $COMPRESSION + $CIFAR + $MAG
+    MAG_NAME = "$COMPRESSION$CIFAR$MAG"
     python main.py --model-class lottery --model vgg16 --dataset cifar10 --experiment multishot --pruner mag --compression $COMPRESSION --pre-epochs 200 --expid $MAG_NAME --post-epoch 100 --level-list 1
 done
