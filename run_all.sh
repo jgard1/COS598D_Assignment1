f6402e173ece7b03b61649592d1c7f34a572d803
Ubuntu <josh@cos598.z0hygjmdoike3opjuazxr0vsoh.bx.internal.cloudapp.net>
2021-03-05 12:55:41 +0000

    "commiting run_all.sh which is just the bash script"

--- run_all.sh
+++ run_all.sh
@@ -1,29 +1,27 @@
 #!/usr/bin/env bash
 
-CIFAR = "cifar_"
-MNIST = "mnist_"
+cifar_str="cifar_"
+mnist_str="mnist_"
 
 # experiment 1 
-for PRUNER_NAME in "rand" "snip" "grasp" "synflow"
+for pruner_name in "rand" "snip" "grasp" "synflow"
 do
-  # python main.py --model-class lottery --model vgg16 --dataset cifar10 --experiment singleshot --pruner synflow --compression 1 --expid $CIFAR$PRUNER_NAME --post-epoch 100
-  NAME = "$MNIST$PRUNER_NAME"
-  python main.py --model-class default --model fc --dataset mnist --experiment singleshot --pruner synflow --compression 1 --expid $NAME --post-epoch 10
+  expid_name="1_${mnist_str}${pruner_name}"
+  pruner_str="${pruner_name}"
+  python main.py --model-class default --model fc --dataset mnist --experiment singleshot --pruner ${pruner_str} --compression 1 --expid ${expid_name} --post-epoch 10
 done
 # have to deal with magnitue pruning seperately because the pre-epochs needs to be set to 200
-# python main.py --model-class lottery --model vgg16 --dataset cifar10 --experiment multishot --pruner mag --compression 1 --pre-epochs 200 cifar_mag
-python main.py --model-class default --model fc --dataset mnist --experiment multishot --pruner-list mag --compression-list 1 --pre-epochs 200 --expid mnist_mag --post-epoch 10 --level-list 1
+python main.py --model-class default --model fc --dataset mnist --experiment multishot --pruner-list mag --compression-list 1 --pre-epochs 200 --expid 1_mnist_mag --post-epoch 10 --level-list 1
 
 # second set of experiments 
-MAG = "mag"
-for COMPRESSION in "0.05" "0.1" "0.2" "0.5" "1" "2"      ### Outer for loop ###
+mag="mag"
+for compression in "0.05" "0.1" "0.2" "0.5" "1" "2"      ### Outer for loop ###
 do
-
-    for PRUNER_NAME in "rand" "snip" "grasp" "synflow" ### Inner for loop ###
+    for pruner_name in "rand" "snip" "grasp" "synflow" ### Inner for loop ###
     do
-          NAME = "$COMPRESSION$CIFAR$PRUNER_NAME"
-          python main.py --model-class lottery --model vgg16 --dataset cifar10 --experiment singleshot --pruner synflow --compression $COMPRESSION --expid $NAME --post-epoch 100
+          expid_name="${compression}_${cifar_str}${pruner_name}"
+          python main.py --model-class lottery --model vgg16 --dataset cifar10 --experiment singleshot --pruner ${pruner_name} --compression ${compression} --expid ${expid_name} --post-epoch 100
     done
-    MAG_NAME = "$COMPRESSION$CIFAR$MAG"
-    python main.py --model-class lottery --model vgg16 --dataset cifar10 --experiment multishot --pruner mag --compression $COMPRESSION --pre-epochs 200 --expid $MAG_NAME --post-epoch 100 --level-list 1
+    mag_name="${compression}_${cifar_str}${mag}"
+    python main.py --model-class lottery --model vgg16 --dataset cifar10 --experiment multishot --pruner mag --compression-list ${compression} --pre-epochs 200 --expid ${mag_name} --post-epoch 100 --level-list 1
 done
